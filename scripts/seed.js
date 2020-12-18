9aa6ca41fe626e84861851771dbbf1e4c851ded9
Emilio Conde <condeemilio891@gmail.com>
2020-12-17 21:07:17 -0600

    heroku buld

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,7 +5,13 @@ const db = require("../models");
 
 mongoose.connect(
     process.env.MONGODB_URI ||
-    "mongodb://localhost/googlebooks"
+    "mongodb://localhost/googlebooks",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
 );
 
 const bookSeed = {
